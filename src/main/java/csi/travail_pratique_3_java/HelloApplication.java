73ea9badd584f060579c0c538600a332fa5f9f42
Mamadou <tiecourafofana23@gmail.com>
2026-05-08 19:51:05 -0400

    interface menu et création du fichier css

--- src/main/java/csi/travail_pratique_3_java/HelloApplication.java
+++ src/main/java/csi/travail_pratique_3_java/HelloApplication.java
@@ -8,10 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage stage;
+    private static final int LARGEUR_APPLICATION = 600;
+    private static final int HAUTEUR_APPLICATION = 400;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), LARGEUR_APPLICATION, HAUTEUR_APPLICATION);
+
+        HelloApplication.stage = stage;
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/csi/travail_pratique_3_java/HelloController.java
+++ src/main/java/csi/travail_pratique_3_java/HelloController.java
@@ -1,14 +1,41 @@
 package csi.travail_pratique_3_java;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button boutonBalleRebon;
+
+    @FXML
+    private Button boutonMotsCache;
+
+    @FXML
+    private Button boutonQuitter;
+
+    @FXML
+    private Button boutonReglement;
+
+    @FXML
+    void onBoutonBalleRebonClic(ActionEvent event) {
+
     }
+
+    @FXML
+    void onBoutonMotsCacheClic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBoutonQuitter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBoutonReglements(ActionEvent event) {
+
+    }
+
 }
