fdfc947ff9ffd8e3144d8eaf302fbdffbb522571
Mamadou <tiecourafofana23@gmail.com>
2026-05-15 16:24:04 -0400

    l'interface du jeu de mots caché

---
+++ src/main/java/csi/travail_pratique_3_java/ControllerMotsCaches.java
@@ -0,0 +1,72 @@
+package csi.travail_pratique_3_java;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.media.MediaView;
+
+public class ControllerMotsCaches {
+
+    @FXML
+    private Button boutonSuivant;
+
+    @FXML
+    private Button boutonValider;
+
+    @FXML
+    private TextField champReponse;
+
+    @FXML
+    private ComboBox<?> comboTheme;
+
+    @FXML
+    private Label labelEssais;
+
+    @FXML
+    private Label labelIndice;
+
+    @FXML
+    private Label labelMessage;
+
+    @FXML
+    private Label labelMotCache;
+
+    @FXML
+    private Label labelScore;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    void onChangerTheme(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onJouerMusique(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onJouerVideo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMotSuivant(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onValider(ActionEvent event) {
+
+    }
+
+
+
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module csi.travail_pratique_3_java {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.naming;
+    requires javafx.media;
 
 
     opens csi.travail_pratique_3_java to javafx.fxml;
